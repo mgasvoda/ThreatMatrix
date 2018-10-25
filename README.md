a112cf05f84d9e296088ea5b82783a377f6bef15
Michael Gasvoda <mgasvoda01@gmail.com>
2018-10-25 16:08:18 -0400

    initial clean commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to ThreatMatrix
+
+This project exists primarily to explore various analytical methodologies and tools in the context of global conflict data. 
---
+++ threatmatrix_frontend/app/processing.py
@@ -0,0 +1,110 @@
+import csv 
+import sqlalchemy
+import os
+import folium
+import pandas as pd
+import cgi
+import re
+
+from bokeh.models.widgets import RangeSlider, Button, DataTable, TableColumn, NumberFormatter
+from bokeh.models import ColumnDataSource, Whisker
+from bokeh.plotting import figure, show
+from bokeh.layouts import row, widgetbox
+from db import db_location
+
+
+def get_data(n): 
+    engine = sqlalchemy.create_engine(db_location)
+    conn = engine.connect()
+
+    df = pd.read_sql('events', conn)[:n]
+    df['notes'] = df['notes'].map(clean_note)
+    return df 
+
+
+def clean_note(note):
+    note = note.replace("'", "").replace('"', '')
+
+    if len(note) > 100:
+        parts = []
+        for i in range(0, len(note.split()), 10):
+            parts.append(' '.join(note.split()[i:i+10]))
+        note = '<br>'.join(parts)
+    return note
+
+
+def create_map(df, map_type):
+    if map_type == 'choropleth':
+        create_choropleth(df, columns['fatalities', 'country'])
+    elif map_type == 'points':
+        create_points(df)
+
+
+def create_points(df):
+    m = folium.Map(location=[35, 55], zoom_start=5)
+
+    points = df[['latitude', 'longitude', 'notes']].values.tolist() 
+
+    for i in range(len(points)):
+        folium.Marker([float(points[i][0]), float(points[i][1])], tooltip=points[i][2]).add_to(m)
+
+    m.save('maps/points.html')
+
+
+def create_choropleth(df, columns):
+    country_geo = 'assets/countries.geojson'
+    m = folium.Map(location=[0, 0], zoom_start=1)
+    m.choropleth(
+        geo_data=country_geo,
+        data=df,
+        columns=columns,
+        key_on='properties.ADMIN',
+        fill_color="YlGn",
+        fill_opacity=0.7,
+        legend_name='GDP per Capita'
+    )
+
+    folium.LayerControl().add_to(m)
+
+    m.save('maps/choropleth.html')
+
+
+def create_bar_chart(df):
+    df['fatalities'] = pd.to_numeric(df['fatalities'])
+    df['fatalities'] = df['fatalities'].fillna(0)
+
+    sums = df.groupby('iso3')['fatalities'].sum().sort_values()
+    sums = sums[sums > 0]
+    countries = sums.index.values
+    counts = sums.values
+
+    p = figure(x_range=countries, plot_height=250, title="Fatalities by Country")
+    p.vbar(x=countries, top=counts, width=0.5)
+    p.y_range.start = 0
+    p.xgrid.grid_line_color = None
+
+    return p
+
+def create_table(df):
+    source = ColumnDataSource(data=dict())
+    current = df.sort_values('fatalities', ascending=False).head(10)
+
+    source.data = {
+        'Country'             : current.country,
+        'Date'                : current.event_date,
+        'Fatalities'          : current.fatalities,
+        'Description'         : current.notes,
+    }
+
+    columns = [
+        TableColumn(field="Country", title="Country"),
+        TableColumn(field="Date", title="Date"),
+        TableColumn(field="Fatalities", title="Fatalities"),
+        TableColumn(field="Description", title="Description")
+    ]
+
+    data_table = DataTable(source=source, columns=columns, width=800)
+
+    table = widgetbox(data_table)
+
+    return table
